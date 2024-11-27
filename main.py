90e16d17de5e891a1f3ba1bbb53c0f9a86d46eae
aditya <adityanavaneethan98@gmail.com>
2024-11-27 15:32:40 +0000

    new files

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from datetime import date
+import pymysql
+pymysql.install_as_MySQLdb()
 from flask import Flask, abort, render_template, redirect, url_for, flash
 from flask_bootstrap import Bootstrap5
 from flask_ckeditor import CKEditor
